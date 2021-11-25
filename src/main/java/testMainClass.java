50dd5509270062f2050a037bd12b657f0726d640
Kamen Simeonov <kamen@solarmd.co.za>
2021-11-25 08:13:15 +0200

    new

--- src/main/java/testMainClass.java
+++ src/main/java/testMainClass.java
@@ -1,13 +1,13 @@
 
 import java.util.Scanner;
 
-
 public class testMainClass {
-    
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         System.out.println(" User input is :  " + input);
+        System.out.println("Test repository");
     }
 
 }
