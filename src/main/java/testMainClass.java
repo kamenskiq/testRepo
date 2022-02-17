70be953e500c640d93f17a9875a96e6253f792fd
Kamen Simeonov <70510696+kamenskiq@users.noreply.github.com>
2022-02-17 10:42:46 +0200

    new

--- src/main/java/testMainClass.java
+++ src/main/java/testMainClass.java
@@ -8,6 +8,7 @@ public class testMainClass {
         short b = 5243;
         long c = ((a & 0xFFFF) << 16 | (b & 0xFFFF));
         //((bb.getShort() & 0xFFFF) << 16) | (bb.getShort() & 0xFFFF);
+        //changes for test
         System.out.println("INT IS : " + c);
     }
 
