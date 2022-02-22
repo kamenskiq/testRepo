162cecb947ff4dc69d2b8418771e28aea8a4de2c
Kamen Simeonov <70510696+kamenskiq@users.noreply.github.com>
2022-02-22 11:10:23 +0200

    add print line

--- src/main/java/ConditionalStatements.java
+++ src/main/java/ConditionalStatements.java
@@ -11,34 +11,24 @@ public class ConditionalStatements {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        double budget = Double.parseDouble(scanner.nextLine());
+        int numStat = Integer.parseInt(scanner.nextLine());
         double price = Double.parseDouble(scanner.nextLine());
-        int puz = Integer.parseInt(scanner.nextLine());
-        int kukla = Integer.parseInt(scanner.nextLine());
-        int bear = Integer.parseInt(scanner.nextLine());
-        int minon = Integer.parseInt(scanner.nextLine());
-        int truck = Integer.parseInt(scanner.nextLine());
-        double profit = (puz * 2.60) + (kukla * 3) + (bear * 4.10) + (minon * 8.20) + (truck * 2);
-        double naem;
-        int numToys = puz + kukla + bear + minon + truck;
-
-        double discount;
-
-        if (numToys >= 50) {
-            discount = (profit * 0.25);
-            profit = profit - discount;
-            naem = (profit * 0.1);
-            profit = profit - naem;
-
-        } else {
-            naem = profit * 0.1;
-            profit = profit - naem;
+double totalPrice;
+        double decor = budget * 0.1;
+        if (numStat > 150) {
+           totalPrice = numStat * (price - (price * 0.1));
         }
-        if (profit <= price) {
-            System.out.format("Not enough money! %.2f lv needed. ", (price - profit));
+        else {
+            totalPrice = numStat * price;
         }
-        if (profit >= price) {
-            System.out.format("Yes! %.2f lv left. ", (profit - price));
-
+        if ((totalPrice + decor) > budget) {
+            System.out.println("Not enough money!");
+            System.out.format("Wingard needs %.2f leva more.", ((totalPrice + decor) - budget));
+        }
+        if ((totalPrice + decor) <= budget) {
+            System.out.println("Action!");
+            System.out.format("Wingard starts filming with %.2f leva left.", (budget - (totalPrice + decor)));
         }
     }
 
