24fe04722e7b1e52b0b04a17cdfa7b3d40568293
Kamen Simeonov <70510696+kamenskiq@users.noreply.github.com>
2022-02-25 10:35:42 +0200

    add print line

--- src/main/java/ConditionalStatements.java
+++ src/main/java/ConditionalStatements.java
@@ -11,25 +11,11 @@ public class ConditionalStatements {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        double recordSec = Double.parseDouble(scanner.nextLine());
+        double m = Double.parseDouble(scanner.nextLine());
         double budget = Double.parseDouble(scanner.nextLine());
-        int numStat = Integer.parseInt(scanner.nextLine());
-        double price = Double.parseDouble(scanner.nextLine());
-double totalPrice;
-        double decor = budget * 0.1;
-        if (numStat > 150) {
-           totalPrice = numStat * (price - (price * 0.1));
-        }
-        else {
-            totalPrice = numStat * price;
-        }
-        if ((totalPrice + decor) > budget) {
-            System.out.println("Not enough money!");
-            System.out.format("Wingard needs %.2f leva more.", ((totalPrice + decor) - budget));
-        }
-        if ((totalPrice + decor) <= budget) {
-            System.out.println("Action!");
-            System.out.format("Wingard starts filming with %.2f leva left.", (budget - (totalPrice + decor)));
-        }
+        int mPerSec = Integer.parseInt(scanner.nextLine());
+        
     }
 
 }
