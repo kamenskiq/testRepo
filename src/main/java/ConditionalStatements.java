6c7b3c39b1ab43fbf83c9498240d9b9b635431f8
Kamen Simeonov <70510696+kamenskiq@users.noreply.github.com>
2022-02-28 14:49:04 +0200

    new code

--- src/main/java/ConditionalStatements.java
+++ src/main/java/ConditionalStatements.java
@@ -10,35 +10,24 @@ public class ConditionalStatements {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String in = s.nextLine();
-        int count = 0;
-        for (int i = 0; i < in.length(); i++) {
-            char n = in.charAt(i);
-
-            switch (n) {
-                case 'a': {
-                    count += 1;
+        int hours = Integer.parseInt(s.nextLine());
+        String day = s.nextLine();
+        switch (day) {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+            case "Saturday": {
+                if (hours < 10 || hours > 18) {
+                    System.out.println("closed");
+                } else {
+                    System.out.println("open");
                 }
-                break;
-                case 'e': {
-                    count += 2;
-                }
-                break;
-                case 'i': {
-                    count += 3;
-                }
-                break;
-
-                case 'o': {
-                    count += 4;
-                }
-                break;
-                case 'u': {
-                    count += 5;
-                }
-                break;
+            }
+            case "Sunday": {
+                System.out.println("closed");
             }
         }
-        System.out.println(count);
     }
 }
